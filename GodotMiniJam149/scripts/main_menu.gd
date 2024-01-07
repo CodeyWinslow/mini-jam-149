cfdcb6e260fb92eefb159d5df58dc74784629810
Iza-Cowie <72038782+Iza-Cowie@users.noreply.github.com>
2024-01-07 13:07:08 -0800

    Added pause menu

---
+++ GodotMiniJam149/scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	print("loaded main menu")
+
+func _on_start_game_pressed():
+	print("start game pressed")
+	get_tree().change_scene_to_file("res://scenes/iza_movement_test.tscn")
+
+func _on_credits_pressed():
+	print("credits pressed")
+	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
+
+func _on_quit_pressed():
+	print("quit pressed")
+	get_tree().quit()
