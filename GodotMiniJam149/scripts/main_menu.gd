045bed1f7e27417e06f3b51015fc4af47f824012
Codey Winslow <codeywinslow@gmail.com>
2024-01-07 15:23:42 -0800

    load gameplay scene when start game pressed

--- GodotMiniJam149/scripts/main_menu.gd
+++ GodotMiniJam149/scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func _on_start_game_pressed():
 	print("start game pressed")
-	get_tree().change_scene_to_file("res://scenes/iza_movement_test.tscn")
+	get_tree().change_scene_to_file("res://scenes/scene_gameplay.tscn")
 
 func _on_credits_pressed():
 	print("credits pressed")
