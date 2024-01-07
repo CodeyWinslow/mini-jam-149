d965cc6482b661d853f53bf28dc32757131e90d4
Codey Winslow <codeywinslow@gmail.com>
2024-01-06 20:41:08 -0800

    Add WASD and controller input support. Update movement script to use new input actions. Add anim mannequin prefab for testing animations.

--- GodotMiniJam149/scripts/test/mail_man_animation_test.gd
+++ GodotMiniJam149/scripts/test/mail_man_animation_test.gd
@@ -5,6 +5,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if AnimNode == null:
+		AnimNode = get_node("MailMan/AnimationPlayer") as AnimationPlayer;
+	
 	if AnimNode != null:
 		AnimNode.play(AnimationToPlay)
 
