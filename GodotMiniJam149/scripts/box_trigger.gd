24396f87b11a47a2ca06bddace3105ee014ddb31
Codey Winslow <codeywinslow@gmail.com>
2024-01-07 19:38:22 -0800

    send box with signals on box trigger

--- GodotMiniJam149/scripts/box_trigger.gd
+++ GodotMiniJam149/scripts/box_trigger.gd
@@ -2,8 +2,8 @@ extends Area3D
 
 const BoxGroupName = "box"
 
-signal box_entered
-signal box_exited
+signal box_entered(box : Node3D)
+signal box_exited(box : Node3D)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +17,9 @@ func _process(delta):
 
 func _on_body_entered(body : Node3D):
 	if body.is_in_group(BoxGroupName):
-		box_entered.emit()
+		box_entered.emit(body)
 
 
 func _on_body_exited(body):
 	if body.is_in_group(BoxGroupName):
-		box_exited.emit()
+		box_exited.emit(body)
